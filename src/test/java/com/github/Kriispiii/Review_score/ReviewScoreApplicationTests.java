ef8ce05293f45fef25355b64f9259129c15e9cec
Krispi <nkoosta2014@yandex.ru>
2022-07-05 05:18:30 +0300

    0

---
+++ src/test/java/com/github/Kriispiii/Review_score/ReviewScoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.Kriispiii.Review_score;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReviewScoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
